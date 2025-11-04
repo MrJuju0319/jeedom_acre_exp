ce2dfdafff298994c30e6303387cdd59bdbd26a9
MrJuju0319 <16257583+MrJuju0319@users.noreply.github.com>
2025-11-04 14:50:28 +0100

    Load Jeedom core before Acreexp AJAX handling

--- core/ajax/acreexp.ajax.php
+++ core/ajax/acreexp.ajax.php
@@ -15,6 +15,7 @@
  * along with Jeedom.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+require_once __DIR__ . '/../../../../core/php/core.inc.php';
 require_once __DIR__ . '/../class/acreexp.class.php';
 
 try {
